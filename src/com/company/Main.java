859ef03c68f2406c46476cb72959c4a6fea77e41
Sydbob <kiva1102@Gmail.com>
2017-11-20 15:50:11 +0000

    changes
    ...

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,32 +8,28 @@ public class Main {
 
     public static void main(String[] args) throws FileNotFoundException
     {
+        //try to avoid absolute paths... have the txt file where exe is located
+        //ask about exe location in java!!!!!!!!
         Scanner in = new Scanner(new FileReader("D:\\Java Projects\\IO-NotAssesed-Java\\data.txt"));
-        //Scanner in2 = new Scanner(new FileReader("D:\\Java Projects\\IO-NotAssesed-Java\\data.txt"));
         PrintWriter outFile = new PrintWriter("D:\\Java Projects\\IO-NotAssesed-Java\\dictionary.txt");
 
-        String inputText = "";
+        String [] inputWords;
         ArrayList<String> finalWords = new ArrayList<String>();
 
-        //print whole contents of a file to an input string
+        //pick out words from the read file string by string
         while (in.hasNextLine())
         {
-            inputText += in.nextLine();
-        }
-
-        //use regex to split inputString into words (for now only 'whole words')
-        String [] words = inputText.split("\\W+");
-
-        //take out actual words, add them to the arrayList "finalWords"
-        for (String word: words)
-        {
-            //check if each string doesn't contain a digit or a special character
-            if (!word.matches(".*\\d.*") && !word.matches("[^A-za-z]"))
+            //split a readin string into array of words (String)
+            inputWords = in.nextLine().split("\\W+");
+            //take out actual (English) words, add them to the arrayList "finalWords"
+            for (String word: inputWords)
             {
-                finalWords.add(word.toLowerCase());
+                if (!word.matches(".*\\d.*") && !word.matches("[^A-za-z]"))
+                {
+                    finalWords.add(word.toLowerCase());
+                }
             }
         }
-
         //sorting finalWords alphabetically
         Collections.sort(finalWords);
 
@@ -45,7 +41,6 @@ public class Main {
             if ((i+1) < finalWords.size() && !finalWords.get(i).equals(finalWords.get(i+1)))
             {
                 outFile.println(finalWords.get(i) + Gaps(finalWords.get(i)) + counter);
-                //some method that decides amount of gaps to print instead of single  " ";
                 counter = 1;
             }
             else
